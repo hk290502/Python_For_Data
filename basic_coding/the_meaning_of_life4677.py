670ab6e5ba77b864d3a580ce33cdcc356c587300
Hamza Khan <hamza.khan@envitia.com>
2026-02-08 20:54:09 +0000

    Initial commit

---
+++ basic_coding/the_meaning_of_life4677.py
@@ -0,0 +1,22 @@
+person_name = str(input("What is your name? "))
+life_meaning = str(input("What is the meaning of life?" ))
+print("Hello " + person_name + ", What is the meaning of life? " + life_meaning)
+
+print("\n SUMMARY OF RESULTS \n")
+
+print("Name ==> " + person_name + "\n")
+
+print("Meaning of life ==> " + life_meaning + "\n")
+
+## first get your user's name
+#person_name = input("What is your name? ")
+
+# say hello, and ask the all-important question
+#life_meaning = input("Hello, " + person_name + ". What is the meaning of life? ")
+
+# print this out
+#print("\nSUMMARY OF RESULTS\n")
+
+#print("Name ==> " + person_name + "\n")
+#print("The meaning of life ==>" + life_meaning)
+
