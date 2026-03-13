d2536eff5e6c353d112d42f0b40c958b002a4fa0
Hamza Khan <hamza.khan@envitia.com>
2026-03-13 16:13:36 +0000

    slicing ex 2

---
+++ slicing/analysing_common_words4691.py
@@ -0,0 +1,62 @@
+# list of common words (source: Collins Dictionary)
+common_words = ("the", "of", "and", "a", "to", \
+    "in", "is", "you", "that", "it", "he", \
+    "was", "for", "on", "are", "as", "with", "his", \
+        "they", "I", "at", "be", "this", "have", "from")
+
+# print out this list
+title = "The original words"
+print(title)
+print(common_words)
+
+
+# how many are there?
+title = "A - Number of words"
+print(len(common_words))
+
+
+
+# show the first 5 words
+title = "B - First 5 words"
+print(common_words[:5])
+
+
+
+# the last 3 words
+title = "C - Last 3 words"
+print(common_words[-3:])
+
+
+# every fifth word
+title = "D - Every 5th word"
+print(common_words[::5])
+
+
+
+
+# iterate over words, listing out the ones with four letters
+title = "E - Four-letter words"
+for word in common_words:
+    if len(word) == 4:
+        print(word)
+    
+
+
+
+
+
+# words beginning with W
+title = "F - Words starting with W"
+for word in common_words:
+    if word.startswith("w"):
+        print(word)
+
+    
+
+
+
+
+# show the words in alphabetical order (need to convert to a list first)
+title = "G - Words in order"
+print(sorted(common_words))
+
