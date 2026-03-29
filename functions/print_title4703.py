6172e6bbb19723d7cbf650062f5f9d4a11b970ce
Hamza Khan <hamza.khan@envitia.com>
2026-03-29 18:22:39 +0100

    functions1

---
+++ functions/print_title4703.py
@@ -0,0 +1,19 @@
+# display title, underlined
+
+def print_title(text):
+    print(text)
+    print("*" * len(text))
+
+first_title = "Interesting facts about Hadfield"
+
+print(first_title)
+
+print_title(first_title)
+
+# list interesting facts
+
+print("The League of Gentlemen was filmed there")
+
+print("It's where Wise Owl Training are based")
+
+print("Er ... that might be it!")
